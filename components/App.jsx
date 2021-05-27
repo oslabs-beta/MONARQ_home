65667dde418603bc93ba4037955fdeadf7fec88d
Peter B <63689434+Peter-Ba@users.noreply.github.com>
2021-05-27 19:38:23 -0400

    laid out home page component structure and bones of the page

--- components/App.jsx
+++ components/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
 import Visualizer from "./Visualizer";
 
 function App() {
   return (
     <ChakraProvider>
-      <Visualizer />
+      <Header />
+      <Body />
+      <Footer />
     </ChakraProvider>
   );
 }
+
 export default App;
