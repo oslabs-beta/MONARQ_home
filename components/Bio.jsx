70d50bcd0aaf8ca2c6a95df227180e59d84bacb2
Peter B <63689434+Peter-Ba@users.noreply.github.com>
2021-05-28 18:42:02 -0400

    Created Bio component and migrated from homepage, adjusted footer and renamed body component to hompage, also refactored to work with the bio component

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -2,18 +2,72 @@ import React from "react";
 import { Button, Flex, Spacer } from "@chakra-ui/react";
 import { GrLinkedin, GrGithub } from "react-icons/gr";
 
-<Flex direction="column" align="center">
-  <h2>Tyler Kneidl</h2>
-  <h2>FullStack Engineer</h2>
-  <a href="https://www.linkedin.com/in/peterbaniuszewicz/">
-    <Button leftIcon={<GrLinkedin />} variant="ghost">
-      LinkedIn
-    </Button>
-  </a>
-  <a href="https://github.com/Peter-Ba">
-    <Button leftIcon={<GrGithub />} variant="ghost">
-      Github
-    </Button>
-  </a>
-</Flex>
-<Spacer />
+const Bio = () => {
+  return (
+    <Flex>
+      <Spacer />
+      <Flex direction="column">
+        <h2>Peter Baniuszewicz</h2>
+        <h2>FullStack Engineer</h2>
+        <a href="https://www.linkedin.com/in/peterbaniuszewicz/">
+          <Button leftIcon={<GrLinkedin />} variant="ghost">
+            LinkedIn
+          </Button>
+        </a>
+        <a href="https://github.com/Peter-Ba">
+          <Button leftIcon={<GrGithub />} variant="ghost">
+            Github
+          </Button>
+        </a>
+      </Flex>
+      <Spacer />
+      <Flex direction="column">
+        <h2>Amy Chen</h2>
+        <h2>FullStack Engineer</h2>
+        <a href="https://www.linkedin.com/in/amyechen">
+          <Button leftIcon={<GrLinkedin />} variant="ghost">
+            LinkedIn
+          </Button>
+        </a>
+        <a href="https://github.com/designal46">
+          <Button leftIcon={<GrGithub />} variant="ghost">
+            Github
+          </Button>
+        </a>
+      </Flex>
+      <Spacer />
+      <Flex direction="column" align="center">
+        <h2>Tyler Kneidl</h2>
+        <h2>FullStack Engineer</h2>
+        <a href="https://www.linkedin.com/in/tylerkneidl/">
+          <Button leftIcon={<GrLinkedin />} variant="ghost">
+            LinkedIn
+          </Button>
+        </a>
+        <a href="https://github.com/tylerkneidl">
+          <Button leftIcon={<GrGithub />} variant="ghost">
+            Github
+          </Button>
+        </a>
+      </Flex>
+      <Spacer />
+      <Flex direction="column">
+        <h2>Helen Regula</h2>
+        <h2>FullStack Engineer</h2>
+        <a href="https://www.linkedin.com/in/helen-regula/">
+          <Button leftIcon={<GrLinkedin />} variant="ghost">
+            LinkedIn
+          </Button>
+        </a>
+        <a href="https://github.com/helenregula">
+          <Button leftIcon={<GrGithub />} variant="ghost">
+            Github
+          </Button>
+        </a>
+      </Flex>
+      <Spacer />
+    </Flex>
+  );
+};
+
+export default Bio;
--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,74 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Flex, Spacer } from "@chakra-ui/react";
-import { GrLinkedin, GrGithub } from "react-icons/gr";
-
+import { Flex } from "@chakra-ui/react";
+import Bio from "./Bio";
 
 const HomePage = () => {
   return (
     <Flex>
       <Link to="/manifest">Manifest Creation</Link>
-      <Spacer />
-      <Flex direction="column">
-        <h2>Peter Baniuszewicz</h2>
-        <h2>FullStack Engineer</h2>
-        <a href="https://www.linkedin.com/in/peterbaniuszewicz/">
-          <Button leftIcon={<GrLinkedin />} variant="ghost">
-            LinkedIn
-          </Button>
-        </a>
-        <a href="https://github.com/Peter-Ba">
-          <Button leftIcon={<GrGithub />} variant="ghost">
-            Github
-          </Button>
-        </a>
-      </Flex>
-      <Spacer />
-      <Flex direction="column">
-        <h2>Amy Chen</h2>
-        <h2>FullStack Engineer</h2>
-        <a href="https://www.linkedin.com/in/peterbaniuszewicz/">
-          <Button leftIcon={<GrLinkedin />} variant="ghost">
-            LinkedIn
-          </Button>
-        </a>
-        <a href="https://github.com/Peter-Ba">
-          <Button leftIcon={<GrGithub />} variant="ghost">
-            Github
-          </Button>
-        </a>
-      </Flex>
-      <Spacer />
-      <Flex direction="column" align="center">
-        <h2>Tyler Kneidl</h2>
-        <h2>FullStack Engineer</h2>
-        <a href="https://www.linkedin.com/in/peterbaniuszewicz/">
-          <Button leftIcon={<GrLinkedin />} variant="ghost">
-            LinkedIn
-          </Button>
-        </a>
-        <a href="https://github.com/Peter-Ba">
-          <Button leftIcon={<GrGithub />} variant="ghost">
-            Github
-          </Button>
-        </a>
-      </Flex>
-      <Spacer />
-      <Flex direction="column">
-        <h2>Helen Regula</h2>
-        <h2>FullStack Engineer</h2>
-        <a href="https://www.linkedin.com/in/peterbaniuszewicz/">
-          <Button leftIcon={<GrLinkedin />} variant="ghost">
-            LinkedIn
-          </Button>
-        </a>
-        <a href="https://github.com/Peter-Ba">
-          <Button leftIcon={<GrGithub />} variant="ghost">
-            Github
-          </Button>
-        </a>
-      </Flex>
-      <Spacer />
+      <Bio />
     </Flex>
   );
 };
