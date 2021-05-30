701f8c44e1a654a3a636306d244390f9a86ccf84
Helen Regula <margaretregula@MacBook-Pro.local>
2021-05-30 12:04:00 -0400

    Theme file and new Webpack for fonts to work

--- theme.js
+++ theme.js
@@ -1,14 +1,28 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   config: {
     // initialColorMode: 'dark'
   },
+  styles: {
+    global: {},
+  },
   colors: {
     brand: {
-      100: "#F56565"
-    }
-  }
-})
+      mainO: "#ED8936",
+      lightO: "#FBD38D",
+      dark0: "#C05621",
+      mainBl: "#4299E1",
+      lightBl: "#BEE3F8",
+      darkBl: "#2C5282",
+      mainBr: "#744210",
+      whiteT: "#EDF2F7",
+    },
+  },
+  fonts: {
+    heading: "Sansita",
+    body: "Manjari",
+  },
+});
 
-export default theme
+export default theme;
